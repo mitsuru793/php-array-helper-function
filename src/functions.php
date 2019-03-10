ea2532f5e2a36611bf2f31237548740ca4b55967
mitsuru793 <mitsuru793@gmail.com>
2019-03-11 01:59:31 +0900

    add typehint

--- src/functions.php
+++ src/functions.php
@@ -101,7 +101,7 @@ if (!function_exists('array_diff_key_recursive')) {
 }
 
 if (!function_exists('array_filter_recursive')) {
-    function array_filter_recursive($array, callable $test = null): array
+    function array_filter_recursive(array $array, callable $test = null): array
     {
         foreach ($array as &$value) {
             if (is_array($value)) {
@@ -117,7 +117,7 @@ if (!function_exists('array_filter_recursive')) {
 }
 
 if (!function_exists('array_pick')) {
-    function array_pick(&$array, array $values): array
+    function array_pick(array &$array, array $values): array
     {
         $new = [];
         $picked = [];
